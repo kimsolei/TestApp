b2e42346ff10cb08dda92052a788000eeb9d56b3
Kim Solei <kimsolei@cmac.org.kh>
2019-08-29 13:27:20 +0700

    Add project files.

---
+++ TestApp/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace TestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Location = new System.Drawing.Point(350, 282);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(777, 499);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1160, 829);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ TestApp/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int row =6, col=7;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Button day = new Button() {
+                        FlatStyle = FlatStyle.Popup,
+                        BackColor = Color.FromArgb(30, 30, 30),
+                        Width = 50,
+                        Height = 50,
+                        Left = j * 50,
+                        Top = i * 50,
+                        Text = i.ToString(),
+                        ForeColor = Color.White
+                    };
+                    day.FlatAppearance.BorderColor = Color.Gray;
+                    day.FlatAppearance.MouseDownBackColor = Color.AliceBlue;
+                    panel1.Controls.Add(day);
+                    if (i * j == 3)
+                    {
+                        day.ForeColor = Color.Blue;
+                        day.BackColor = Color.Yellow;
+                        day.Enabled = false;
+                    }
+                }
+            }
+        }
+    }
+}
